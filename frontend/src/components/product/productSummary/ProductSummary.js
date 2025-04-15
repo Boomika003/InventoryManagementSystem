e6df1ea0581db484c269056d6fc9b2829006db72
Boomika003 <boomikaedu2003@gmail.com>
2025-04-15 21:37:28 +0530

    cloudinary upload

--- frontend/src/components/product/productSummary/ProductSummary.js
+++ frontend/src/components/product/productSummary/ProductSummary.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./ProductSummary.scss";
-import { AiFillDollarCircle } from "react-icons/ai";
+//import { AiFillDollarCircle } from "react-icons/ai";
+import { FaRupeeSign } from "react-icons/fa";
 import { BsCart4, BsCartX } from "react-icons/bs";
 import { BiCategory } from "react-icons/bi";
 import InfoBox from "../../infoBox/InfoBox";
@@ -15,7 +16,7 @@ import {
 } from "../../../redux/features/product/productSlice";
 
 // Icons
-const earningIcon = <AiFillDollarCircle size={40} color="#fff" />;
+const earningIcon = <FaRupeeSign size={40} color="#fff" />;
 const productIcon = <BsCart4 size={40} color="#fff" />;
 const categoryIcon = <BiCategory size={40} color="#fff" />;
 const outOfStockIcon = <BsCartX size={40} color="#fff" />;
@@ -39,7 +40,7 @@ const ProductSummary = ({ products }) => {
 
   return (
     <div className="product-summary">
-      <h3 className="--mt">Inventory Stats</h3>
+      <h3 className="--mt">Inventory Management System</h3>
       <div className="info-summary">
         <InfoBox
           icon={productIcon}
@@ -50,7 +51,7 @@ const ProductSummary = ({ products }) => {
         <InfoBox
           icon={earningIcon}
           title={"Total Store Value"}
-          count={`$${formatNumbers(totalStoreValue.toFixed(2))}  `}
+          count={`₹${formatNumbers(totalStoreValue.toFixed(2))}  `}
           bgColor="card2"
         />
         <InfoBox
